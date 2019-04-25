515ac16269866b42be7fba9be3073d7aa4a883f8
Maxime <33199920+DocMarmel@users.noreply.github.com>
2019-04-25 11:42:42 +0200

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,47 +1,52 @@
-let myMap = L.map('map').setView([47.2186371, -1.5541362], 12);
-let markerClusters = L.markerClusterGroup();
+let Map = function(){
 
-/* Affichage de la carte */
+  let myMap = L.map('map').setView([47.2186371, -1.5541362], 12);
+  let markerClusters = L.markerClusterGroup();
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  minZoom: 2,
-  maxZoom: 20
-}).addTo(myMap);
+  /* Affichage de la carte */
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    minZoom: 2,
+    maxZoom: 20
+  }).addTo(myMap);
 
 
-/* Appelle de l'api JCDecaux */
-ajaxGet("https://api.jcdecaux.com/vls/v1/stations?contract=Nantes&apiKey=49decfbc8866823cb4bfac018d5f24967fc28def", function(reponse){
-  let datas = JSON.parse(reponse);
+  /* Appelle de l'api JCDecaux */
+  ajaxGet("https://api.jcdecaux.com/vls/v1/stations?contract=Nantes&apiKey=49decfbc8866823cb4bfac018d5f24967fc28def", function(reponse){
+    let datas = JSON.parse(reponse);
 
- datas.forEach(function(data){
+    datas.forEach(function(data){
 
-  let markers = [];
-  let posStation = data.position;
-  let marker = L.marker(posStation);
+      let markers = [];
+      let posStation = data.position;
+      let marker = L.marker(posStation);
 
-  markerClusters.addLayer(marker);
+      markerClusters.addLayer(marker);
 
-  marker.addEventListener("click", function(){
-    $("#clickStation").attr('hidden', 'true');
+      marker.addEventListener("click", function(){
+        $("#clickStation").attr('hidden', 'true');
 
-    if(data.available_bikes > 0){
-      $("#reserve").removeAttr('disabled');
-      $("#error").attr('hidden', 'true');
-    }else{
-      $("#error").removeAttr('hidden');
-      $("#reserve").attr('disabled', 'true');
-    }
+        if(data.available_bikes > 0){
+          $("#reserve").removeAttr('disabled');
+          $("#error").attr('hidden', 'true');
+        }else{
+          $("#error").removeAttr('hidden');
+          $("#reserve").attr('disabled', 'true');
+        }
 
-      $("#stationName").html(data.name);
-      $("#stationAddress").html(data.address);
-      $("#stationStatus").html(data.status);
-      $("#availableBikes").html(data.available_bikes);
-      $("#availableBikesStands").html(data.available_bike_stands);
+        $("#stationName").html(data.name);
+        $("#stationAddress").html(data.address);
+        $("#stationStatus").html(data.status);
+        $("#availableBikes").html(data.available_bikes);
+        $("#availableBikesStands").html(data.available_bike_stands);
+      });
+
+      markers.push(marker);
     });
 
-    markers.push(marker);
-  });
+    myMap.addLayer(markerClusters);
+   });
+}
 
-  myMap.addLayer(markerClusters);
- });
+let map = new Map();
